8dac1484097b51211aa89aad58c53ae8dce1553c
arlein015 <rachehua45@gmail.com>
2026-03-18 16:17:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    // Tes infos ici (une seule fois !)
+    apiKey: "TON_API_KEY",
+    authDomain: "TON_PROJET.firebaseapp.com",
+    projectId: "TON_PROJET_ID",
+    storageBucket: "TON_PROJET.appspot.com",
+    messagingSenderId: "TON_SENDER_ID",
+    appId: "TON_APP_ID"
 };
 
-// On initialise une seule fois
+// Initialisation unique
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
